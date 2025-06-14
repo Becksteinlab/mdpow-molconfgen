586efd1f4571e7010bb877e351e923118c02b02c
Oliver Beckstein <orbeckst@gmail.com>
2025-06-14 12:40:59 -0700

    removed V46 bigbox file (not needed)

--- molconfgen/data/files.py
+++ molconfgen/data/files.py
@@ -12,7 +12,6 @@ __all__ = [
     "MDANALYSIS_LOGO",  # example file of MDAnalysis logo
     # V46-2-methyl-1-nitrobenzene files
     "V46_MOL2",
-    "V46_BIGBOX_PDB",
     "V46_PDB",
     "V46_ITP",
     "V46_TOP",
@@ -25,7 +24,6 @@ MDANALYSIS_LOGO = resources.files(__name__) / "mda.txt"
 # V46-2-methyl-1-nitrobenzene files
 V46_DIR = resources.files(__name__) / "V46-2-methyl-1-nitrobenzene"
 V46_MOL2 = V46_DIR / "V46-2-methyl-1-nitrobenzene.mol2"
-V46_BIGBOX_PDB = V46_DIR / "V46_bigbox.pdb"
 V46_PDB = V46_DIR / "V46-2-methyl-1-nitrobenzene.pdb"
 V46_ITP = V46_DIR / "V46-2-methyl-1-nitrobenzene.itp"
 V46_TOP = V46_DIR / "V46.top"
