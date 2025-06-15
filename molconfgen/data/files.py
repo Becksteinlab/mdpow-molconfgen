bb2f84f67600b1fa77d685cf4d92a19e9e8056bc
Oliver Beckstein <orbeckst@gmail.com>
2025-06-14 18:37:48 -0700

    add V36 methylacetate files

--- molconfgen/data/files.py
+++ molconfgen/data/files.py
@@ -14,6 +14,10 @@ __all__ = [
     "V46_PDB",
     "V46_ITP",
     "V46_TOP",
+    # V36-methylacetate files
+    "V36_PDB",
+    "V36_ITP",
+    "V36_TOP",
 ]
 
 import importlib.resources as resources
@@ -24,3 +28,9 @@ V46_MOL2 = V46_DIR / "V46-2-methyl-1-nitrobenzene.mol2"
 V46_PDB = V46_DIR / "V46-2-methyl-1-nitrobenzene.pdb"
 V46_ITP = V46_DIR / "V46-2-methyl-1-nitrobenzene.itp"
 V46_TOP = V46_DIR / "V46.top"
+
+# V36-methylacetate files
+V36_DIR = resources.files(__name__) / "V36-methylacetate"
+V36_PDB = V36_DIR / "V36.pdb"
+V36_ITP = V36_DIR / "V36-methylacetate.itp"
+V36_TOP = V36_DIR / "V36.top"
