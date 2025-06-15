d33b1fe690645d9581b706b0bf9cf49ee9049e82
Oliver Beckstein <orbeckst@gmail.com>
2025-06-14 18:50:01 -0700

    test sampler with both V36 and V46

--- molconfgen/tests/test_sampler.py
+++ molconfgen/tests/test_sampler.py
@@ -6,29 +6,36 @@ import numpy as np
 
 from .. import sampler
 from .. import chem
-from ..data.files import V46_PDB, V46_ITP
+from ..data.files import V46_PDB, V46_ITP, V36_ITP, V36_PDB
 
 
-@pytest.fixture
-def universe():
-    """Create a test universe with V46 molecule."""
-    return mda.Universe(V46_ITP, V46_PDB)
+@pytest.fixture(params=[
+    ("V46", V46_ITP, V46_PDB, [[2, 1, 0, 4]]),  # V46 dihedral indices
+    ("V36", V36_ITP, V36_PDB, [[7, 10, 0, 1], 
+                               [10, 0, 1, 3]]),  # V36 dihedral indices
+])
+def molecule_data(request):
+    """Create test universes for both V46 and V36 molecules."""
+    name, itp_file, pdb_file, dihedral_indices = request.param
+    universe = mda.Universe(itp_file, pdb_file)
+    return name, universe, dihedral_indices
 
 
-def test_generate_conformers(universe):
-    """Test generating conformers for V46."""
-    # Create a list of dihedrals for V46
-    dih = universe.atoms[[2, 1, 0, 4]].dihedral
-    dihedrals = [dih]
+def test_generate_conformers(molecule_data, num_conformers=5):
+    """Test generating conformers for V46 and V36 molecules."""
+    name, universe, dihedral_indices = molecule_data
+    
+    # Create a list of dihedrals using a list comprehension
+    dihedrals = [universe.atoms[indices].dihedral for indices in dihedral_indices]
 
     # Convert Universe to RDKit molecule
     mol = chem.load_mol(universe)
 
     # Generate conformers
-    u = sampler.generate_conformers(mol, dihedrals, num=5)
+    u = sampler.generate_conformers(mol, dihedrals, num=num_conformers)
 
     # Check that we got the expected number of conformers
-    assert len(u.trajectory) == 5
+    assert len(u.trajectory) == num_conformers**len(dihedral_indices)
 
     # Check that the molecule is an RDKit molecule
     assert hasattr(mol, "GetNumAtoms")
