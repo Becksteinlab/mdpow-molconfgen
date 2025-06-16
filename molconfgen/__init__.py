b6fb5c4d0be9bd8e0aacdf13488d94459d16b4e9
Oliver Beckstein <orbeckst@gmail.com>
2025-06-16 13:37:10 -0700

    update buildsystem
    - replaced versioneer with versioningit
      - make versioningit generate _version.py
      - ignore _version
      - use [tool.versioningit.write]
    - removed legacy setup.py and setup.cfg
    - use build for package build

--- molconfgen/__init__.py
+++ molconfgen/__init__.py
@@ -5,20 +5,13 @@ mdpow-molconfgen
 Generation of conformers of small molecules.
 """
 
-# Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
-
+# Version is handled by versioningit
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
 
 # Add imports here
 from . import chem
 from . import sampler
 
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
